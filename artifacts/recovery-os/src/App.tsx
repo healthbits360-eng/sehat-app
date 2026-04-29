2b06f398658b640f9e83f7e5d7d0b5f3c46b6232
healthbits360-eng <healthbits360@gmail.com>
2026-04-29 20:21:43 +0530

    Update App.tsx

--- artifacts/recovery-os/src/App.tsx
+++ artifacts/recovery-os/src/App.tsx
@@ -1,11 +1,13 @@
 import { Switch, Route, Router as WouterRouter } from "wouter";
 
 import Landing from "./pages/landing";
+import RoleSelect from "./pages/role-select";
 
 function Router() {
   return (
     <Switch>
       <Route path="/" component={Landing} />
+      <Route path="/role-select" component={RoleSelect} />
     </Switch>
   );
 }
