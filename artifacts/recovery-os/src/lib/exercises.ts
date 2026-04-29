cf7b8a5245ab1ccd782616d7735ada75458c9942
healthbits360-eng <healthbits360@gmail.com>
2026-04-29 22:04:16 +0530

    Create exercises.ts

---
+++ artifacts/recovery-os/src/lib/exercises.ts
@@ -0,0 +1,20 @@
+export const exercises = [
+  {
+    id: 1,
+    name: "Knee Bending",
+    condition: "knee",
+    video: "https://www.youtube.com/embed/1g4mQwF9K0k"
+  },
+  {
+    id: 2,
+    name: "Shoulder Rotation",
+    condition: "shoulder",
+    video: "https://www.youtube.com/embed/BV6b4t0Y6Cs"
+  },
+  {
+    id: 3,
+    name: "Back Stretch",
+    condition: "back",
+    video: "https://www.youtube.com/embed/4BOTvaRaDjI"
+  }
+];  
